8c1c385c04bf117e048118091f8019236165d0b6
gauravyadav0303 <gauravyadav0303@gmail.com>
2023-11-23 05:12:38 +0530

    Animation

--- src/components/Power.jsx
+++ src/components/Power.jsx
@@ -1,5 +1,6 @@
 import { styles } from "../styles";
 import { hand1, hand2, hand3 } from "../assets";
+import { useInView } from "react-intersection-observer";
 import { motion } from "framer-motion";
 import { box13 } from "../assets";
 import gsap from "gsap";
@@ -10,6 +11,13 @@ import { useEffect } from "react";
 gsap.registerPlugin(ScrollTrigger);
 
 function Power() {
+
+    const [ref, inView] = useInView({
+        triggerOnce: false,
+        threshold: 0.2,
+      });
+    
+
   useEffect(() => {
     gsap.to(".box6", {
       scrollTrigger: {
@@ -39,6 +47,7 @@ function Power() {
   return (
     <section
       className={`trigger1 power w-full relative min-h-[80vh]  md:min-h-[90vh] mx-auto`}
+      ref={ref}
     >
       <motion.img
         className="box box7 absolute top-[80vh] right-[10vw] xl:top-[75vh] xl:right-[20vw] rotate-[-40.33]  hidden md:inline-block"
@@ -47,12 +56,41 @@ function Power() {
       />
 
       <div className={` w-full flex flex-col md:flex-row justify-between`}>
-        <div className=" w-full flex flex-col items-center md:w-[60%] h-[20rem] md:h-full lg:h-full  xl:w-[60%]">
-          {/* <img src={hand1} className="absolute hidden" alt="" />
-          <img src={hand2} className="absolute h-[90vh]" alt="" /> */}
-          <img src={hand3} className=" h-[35vh] md:h-[30vh] lg:h-[80vh] xl:h-[90vh]" alt="" />
-        </div>
-        <div className="pb-[50px] lg:pt-[50px] lg:pb-[50px] xl:pt-[120px] xl:pb-[120px] w-full lg:w-[40%] flex flex-col justify-between lg:flex-row items-center">
+       
+          <div className="relative w-full flex flex-col items-center md:w-[60%] h-[20rem] md:h-full lg:h-full xl:w-[40%]">
+            {/* <img src={hand1} className="absolute hidden" alt="" /> */}
+            {inView && (
+            <motion.img
+              src={hand2}
+              initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{
+              duration: 0.8,
+              delay: 0.5,
+              ease: [0, 0.71, 0.2, 1.01]
+            }}
+              className="hand2 md:mt-[5rem] lg:mt-[0rem] absolute md:left-[-3px] xl:left-[-6px] h-[30vh] md:h-[34vh] lg:h-[68vh] xl:h-[76vh]"
+              alt=""
+            />
+             )}
+             {inView && (
+            <motion.img
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{
+              duration: 2,
+              delay: 1.2,
+              ease: [0, 0.71, 0.2, 1.01]
+            }}
+              src={hand3}
+              className="hand3 md:mt-[5rem] lg:mt-[0rem]  absolute md:left-0 h-[35vh] md:h-[40vh] lg:h-[80vh] xl:h-[90vh]"
+              alt=""
+            />
+             )}
+          </div>
+       
+        
+        <div className="pb-[50px] xl:mr-[0rem] lg:pt-[50px] lg:pb-[50px] xl:pt-[120px] xl:pb-[120px] w-full lg:w-[55%] flex flex-col justify-between lg:flex-row items-center">
           <div className="w-[85%] md:p-6 text-center md:text-left">
             <h1 className=" w-full text-[1.7rem] uppercase font-normal">
               “POWER TO THE PEOPLE”
